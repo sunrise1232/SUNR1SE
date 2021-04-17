7c0642df31db6c2af2fbb2a37c6e0c00fda68ce3
yaroslav polyakov <theroyalprime@gmail.com>
2021-04-17 22:07:07 +0300

    123re

---
+++ Storage/PeopleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Storage.Entity;
+
+namespace WebApplication1.Storage
+{
+    public class PeopleContext:DbContext
+    {
+        
+            public PeopleContext(DbContextOptions<PeopleContext> options) : base(options)
+            {
+
+            }
+       public DbSet<Client> clients { get; set; }
+    }
+}
