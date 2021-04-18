109fba5d45f50f46c4b36f086617a850daf70519
yaroslav polyakov <theroyalprime@gmail.com>
2021-04-18 22:04:36 +0300

    clientmanager

---
+++ Manager/Clients/ClientManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Storage;
+using WebApplication1.Storage.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Managers.Clients
+{
+    public class ClientManager: IClientManager
+    {
+        private PeopleContext _ContextClient;
+
+        /* public Client()
+          {
+              _ContextClient = new PeopleContext();
+          }
+        */
+
+        public ClientManager(PeopleContext context)
+        {
+            _ContextClient = context;
+        }
+
+       
+        public void AddClient(Client client)
+        {
+            _ContextClient.clients.Add(client);
+        }
+
+        public ICollection<Client> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Manager/Clients/IClientManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Storage.Entity;
+using WebApplication1.Storage;
+
+namespace WebApplication1.Managers.Clients
+{
+    interface IClientManager
+    {
+        ICollection<Client> GetAll();
+
+        void AddClient(Client Client);
+        //void AuthorizationClient(string login, string password);
+    }
+}
